712c706e8552bd4020cc66641bfe48d8e76b4836
cadu.goncalves <cadu.goncalves@gmail.com>
2022-06-18 15:20:26 -0300

    Fixed workflow

--- code/ansible/entrypoint.sh
+++ code/ansible/entrypoint.sh
@@ -3,8 +3,8 @@
 if [ ! -z "$ANSIBLE_VAULT_PASSWORD" ]
 then
       echo $ANSIBLE_VAULT_PASSWORD >> .vault;
-      ansible-playbook $1 -i $2 --vault-password-file .vault;
+      ansible-playbook $1 -i $2 -e $3 --vault-password-file .vault;
       rm .vault
 else
-      ansible-playbook $1 -i $2
+      ansible-playbook $1 -i $2 -e $3
 fi
